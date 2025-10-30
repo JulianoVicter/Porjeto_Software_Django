4051f7f793b1bb23c529fc2005113261408a6a7d
juliano <testavictr@gmai.com>
2025-10-30 13:55:10 -0300

    Commit inicial

---
+++ projsoft/app_main/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('sobre/', views.sobre, name='sobre'),
+    path('criar-conta/', views.cria_conta, name='cria_conta'),
+]
