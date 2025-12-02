82cf13a42c53d28df2fc18b79282dbcdaf5ac4a1
JULIANO TESTA VICTER <testavicter@gmail.com>
2025-12-02 14:11:30 -0300

    Namespace login URLs in archived project copy

--- untitled folder/contas/urls.py
+++ untitled folder/contas/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = "contas"
+
 urlpatterns = [
     path('', views.escolha_perfil, name='escolha_perfil'),
     path('login/', views.login, name='login'),
-    path('login-ong/', views.login_ong, name='login_ong'),
+    path('login/ong/', views.login, name='login_ong'),
     path('conta/', views.conta, name='conta'),
     path('criar_conta_ong/', views.criar_conta_ong, name='criar_conta_ong'),
     path('escolha_perfil/', views.escolha_perfil, name='escolha_perfil'),
